b1c8e384720681314cd6331278d27094339fa686
Taran Mand <tsmand@yahoo.com>
2020-10-16 18:01:05 +1100

    add Pokemon react comp and graphql query

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_POKEMONS } from '../graphql/getPokemons';
+import Pokemon from '../components/Pokemon';
 
 export default function PokemonsContainer() {
+    
+    let pokemons_array;
+
+    // const {loading, error, data: {pokemons = []} = {}} = useQuery(GET_POKEMONS, {
+    //     variables: {first: 9}
+    // });
+    const { loading, error, data } = useQuery(GET_POKEMONS, {
+        variables: {first: 9}
+    });
+
+    if (data && data.pokemons) {
+        pokemons_array = data.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />);
+    }
+    
+    // console.log(error);
+    // console.log(data);
+    // console.log(loading);
+
+    //if (error) return error.errors.map(e => <h2>Error: {e.message}</h2>);
+
     return (
-        <p>I am a pokemon container</p>  
+        <div className="container">
+            {loading ? <h2>...Loading</h2> : pokemons_array}
+        </div>
     );
 }
